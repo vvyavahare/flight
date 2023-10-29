edeadc623091d4a9d07083dd6cdbe55329d8f9b1
Admin <admin.push@hackerrank.workspace>
2023-10-29 15:42:32 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/main/java/com/hackerrank/api/controller/FlightController.java
+++ src/main/java/com/hackerrank/api/controller/FlightController.java
@@ -1,4 +1,36 @@
 package com.hackerrank.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hackerrank.api.model.Flight;
+import com.hackerrank.api.repository.FlightRepository;
+
+@RestController
 public class FlightController {
+
+  @Autowired
+  private FlightRepository repository;
+
+  @PostMapping("/flight")
+  public ResponseEntity<Flight> createFlight(@RequestBody Flight flight){
+    repository.save(flight);
+    return null;
+  }
+
+  @GetMapping("/flight")
+  public ResponseEntity<List<Flight>> getAllFlights(){
+    return ResponseEntity.ok().body(repository.findAll());
+  }
+
+  @GetMapping("/flight")
+  public ResponseEntity<Flight> findFlightById(Integer id){
+    return ResponseEntity.ok().body(repository.findById(id).get());
+  }
 }
