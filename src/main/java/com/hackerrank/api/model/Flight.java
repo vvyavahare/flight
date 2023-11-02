4f2d78c0f262bbbc24f1e972467aee30410a452e
Vishal V <vishalvyavahare@gmail.com>
2023-11-02 18:42:47 +0100

    refactored code.

--- src/main/java/com/hackerrank/api/model/Flight.java
+++ src/main/java/com/hackerrank/api/model/Flight.java
@@ -14,7 +14,6 @@ public class Flight {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String flight;
     private String origin;
     private String destination;
